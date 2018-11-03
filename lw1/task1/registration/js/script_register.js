3797c77f61f9d991311787c94129c18bf013a185
Ruprecht <43097128+RuprechtKraus@users.noreply.github.com>
2018-11-03 17:16:50 +0300

    Add files via upload

---
+++ lw1/task1/registration/js/script_register.js
@@ -0,0 +1,57 @@
+function checkEmail(str)
+{
+  var result = str.match(/^[0-9a-z-\.]+\@[0-9a-z-]{1,}\.[a-z]{2,}$/i);
+  if (!result)
+  {
+    alert("Введите валидный e-mail.");
+    return false;
+  }
+  return true;
+}
+
+function checkPassword(pass, confrim) 
+{
+  if (pass.length < 6) 
+  {
+    alert("Длина пароля должна быть не менее 6-ти символов.");
+    return false;
+  }
+  if (pass !== confrim) 
+  {
+    alert("Введенные пароли не совпадают.");
+    return false;
+  }
+  return true;
+}
+
+function checkAgreementRules() 
+{
+  var result = document.getElementsByClassName("registration__check_confirmation")[0].checked;
+  if (!result) 
+  {
+    alert("Примите условия соглашения!");
+    return false;
+  }
+  return true;
+}
+
+function registration()
+{
+  var email = document.getElementsByClassName("registration__login")[0].value;
+  var pass = document.getElementsByClassName("password-block__password")[0].value;
+  var passConfirm = document.getElementsByClassName("password-block__password_repeat")[0].value;
+  if (checkEmail(email) && checkPassword(pass, passConfirm) && checkAgreementRules())
+  {
+    alert("Регистрация прошла успешно!");
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+document.getElementsByClassName("registration")[0].onsubmit = function() 
+{
+ return registration();
+};
